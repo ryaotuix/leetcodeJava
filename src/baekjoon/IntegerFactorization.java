963b3a401265f033c708c38af7d4763807afab09
ryaotuix <ryaotuix@naver.com>
2022-10-09 14:41:10 -0400

    upload

---
+++ src/baekjoon/IntegerFactorization.java
@@ -0,0 +1,14 @@
+package baekjoon;
+// # 11653
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class IntegerFactorization {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        List<Integer> factors = new ArrayList<>();
+        
+    }
+}
